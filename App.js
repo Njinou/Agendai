6a47231bc42ecdabea009ace540cd1ad78c03924
Njinou <nitcheupascal@gmail.com>
2021-01-29 23:28:59 -0500

    creating Component for Hidding Keyboard Input

--- App.js
+++ App.js
@@ -1,26 +1,13 @@
 import React, { useState } from "react";
 import {
-  Alert,
-  Modal,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View,
   SafeAreaView,
-  Image,
-  ImagePropTypes
 } from "react-native";
 
 import ModalComposantScreen from './src/views/screens/ModalComposantScreen';
 import MonthScreen from './src/views/screens/MonthScreen';
 import DateTemps from './src/views/screens/DateTemps';
-/*
+import AgTextInput from './src/views/components/AgTextInput';
 
- <MonthScreen/>
-    <View style={{zIndex:1}}>
-    <ModalComposantScreen/>
-    </View>
-*/
 const App: () => React$Node = () => {
   return (
     <SafeAreaView style={{flex:1}}>
@@ -30,4 +17,3 @@ const App: () => React$Node = () => {
 };
 
 export default App;
-// <Image source={require('./src/image/plus/plusplusbleu.png')} style={{position: "absolute", bottom: 0, right: 0,width:150,height:150}}/>
